726f93ada7b161607771ed0be35e12aee63a5cd8
admin admin <evaladmin@example.com>
2020-04-08 23:51:17 +0000

    User admin wrote content /static-assets/js/synchronize.js

--- static-assets/js/synchronize.js
+++ static-assets/js/synchronize.js
@@ -1,39 +1,22 @@
-var url = "studio/api/1/services/api/1/content/write-content.json?site=shopify&phase=onSave&path=/site/components/stores/test.xml&fileName=test.xml&user=admin&contentType=/component/store&unlock=true&nocache=Wed Apr 08 2020 14:46:56 GMT-0600 (Central Standard Time)";
-var header= {"X-XSRF-TOKEN":"597eb1d7-7f06-4853-8cd4-7f801b0eeaac", "Accept": "application/json"};
-var dataType = "json";
-var data = "<component>"+
-	"<content-type>/component/store</content-type>"+
-	"<display-template/>"+
-	"<no-template-required/>"+
-	"<merge-strategy>inherit-levels</merge-strategy>"+
-	"<objectGroupId>e4f1</objectGroupId>"+
-	"<objectId>e4f14314-2de9-0ca1-42e1-2c2d2c3cbc04</objectId>"+
-	"<file-name>e4f14314-2de9-0ca1-42e1-2c2d2c3cbc04.xml</file-name>"+
-	"<internal-name>a</internal-name>"+
-	"<name_s>ab</name_s>"+
-	"<currency_s>USD</currency_s>"+
-	"<createdDate>2020-04-08T16:37:00.267Z</createdDate>"+
-	"<createdDate_dt>2020-04-08T16:37:00.267Z</createdDate_dt>"+
-	"<lastModifiedDate>2020-04-08T16:37:00.267Z</lastModifiedDate>"+
-	"<lastModifiedDate_dt>2020-04-08T16:37:00.267Z</lastModifiedDate_dt>"+
-    "</component>";
-    
+var url = "http://localhost:8080/studio/api/1/services/api/1/security/login.json";
+var data = {"username":"admin","password":"admin"};
+
 var success = function() {
     alert("success");
 }
 
+$.cookie("XSRF-TOKEN", "A_VALUE");
 $(":button").click(function() {
     $.ajax({
         type: "POST",
         url: url,
         data: data,
         headers: {
-            'X-CSRF-TOKEN':'xxxxxxxxxxxxxxxxxxxx',
-            'Content-Type':'application/xml'
+            'X-XSRF-TOKEN':'A_VALUE',
+            'Content-Type':'application/json'
         },
-        contentType: "application/xml; charset=UTF-8",
-        crossDomain: true,
-        dataType: dataType
+        dataType: "json",
+        success: success
     });
 });
 
